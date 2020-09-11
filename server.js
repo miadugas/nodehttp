b118e903d1f96d77f5d07ee131ff556932a38f3e
Mia <mia@Mias-MacBook-Pro.local>
2020-09-10 19:16:34 -0600

    testing for Postman

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  //   console.log(req);
+
+  //I want go pull out the method to test
+  //console.log(req.method);
+
+  //Pull out more info in destructure
+  const { headers, url, method } = req;
+  console.log(headers, url, method);
   res.end();
 });
 
